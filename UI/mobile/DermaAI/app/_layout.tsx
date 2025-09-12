518754dd684239f11fa94a13a3426eacdc95a9c8
Ngane Emmanuel <emmanuelngane06@gmail.com>
2025-09-12 13:18:07 -0700

    implementing the info screen and its subscreens

--- UI/mobile/DermaAI/app/_layout.tsx
+++ UI/mobile/DermaAI/app/_layout.tsx
@@ -17,6 +17,33 @@ export default function RootLayout() {
           headerBackTitle: "Back",
         }}
       />
+
+      {/* Lesion info detail page */}
+      <Stack.Screen
+        name="lesion/[id]"
+        options={{
+          title: "Lesion information",
+          headerBackTitle: "Back",
+        }}
+      />
+
+      {/* FAQ detail page */}
+      <Stack.Screen
+        name="faq/[id]"
+        options={{
+          title: "FAQ information",
+          headerBackTitle: "Back",
+        }}
+      />
+
+      {/* About detail page */}
+      <Stack.Screen
+        name="about/[id]"
+        options={{
+          title: "About information",
+          headerBackTitle: "Back",
+        }}
+      />
     </Stack>
   );
 }
--- UI/mobile/DermaAI/metro.config.js
+++ UI/mobile/DermaAI/metro.config.js
@@ -4,5 +4,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.resolver.assetExts.push('bin');
+config.resolver.assetExts.push("md");
 
 module.exports = config;
